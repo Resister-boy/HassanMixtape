566e75be24b79e80a3747d10ffd17e6b9985f23c
Resister-boy <hassanpumped17@gmail.com>
2022-05-31 15:52:30 +0900

    Design Modified

---
+++ services/quotes.js
@@ -0,0 +1,96 @@
+const quotes = [
+  {
+    quote: 'Your time is limited, so don’t waste it living someone else’s life.',
+    person: 'Steve Jobs'
+  },
+  {
+    quote: 'When you go through hardships and decide not to surrender, that is strength.',
+    person: 'Arnold Schwarzenegger'
+  },
+  {
+    quote: 'There are better starters than me but I’m a strong finisher.',
+    person: 'Usain Bolt'
+  },
+  {
+    quote: 'I’ve failed over and over and over again in my life and that is why I succeed.',
+    person: 'Michael Jordan'
+  },
+  {
+    quote: 'The people who are crazy enough to think they can change the world are the ones who do.',
+    person: 'Steve Jobs'
+  },
+  {
+    quote: 'It is really clear that the most precious resource we all have is time.',
+    person: 'Steve Jobs'
+  },
+  {
+    quote: 'I want to put a ding in the universe.',
+    person: 'Steve Jobs'
+  },
+  {
+    quote: 'Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.',
+    person: 'Rumi'
+  },
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    person: 'Steve Jobs'
+  },
+  {
+    quote: 'Never lose sight of the face that the most important yard stick to your success is how you treat other people.',
+    person: 'Barbara Bush'
+  },
+  {
+    quote: 'I find that the harder I work, the more luck I seem to have.',
+    person: 'Thomas Jefferson'
+  },
+  {
+    quote: 'If you run you stand a chance of losing, but if you don’t run you’ve already lost.',
+    person: 'Barack Obama'
+  },
+  {
+    quote: 'You must learn a new way to think before you can master a new way to be',
+    person: 'Marianne Williamson'
+  },
+  {
+    quote: 'Things change. And friends leave. Life doesn’t stop for anybody.',
+    person: 'Stephen Chbosky'
+  },
+  {
+    quote: 'In any given moment we have two options: to step forward into growth or step back into safety.',
+    person: 'Abraham Maslow'
+  },
+  {
+    quote: 'That which does not kill us makes us stronger.',
+    person: 'Friedrich Nietzsche'
+  },
+  {
+    quote: 'Many of life’s failures are people who did not realize how close they were to success when they gave up.',
+    person: 'Thomas A. Edison'
+  },
+  {
+    quote: 'Success is going from failure to failure without a loss of enthusiasm.',
+    person: 'Winston Churchill'
+  },
+  {
+    quote: 'Try not to become a man of success but rather try to become a man of value.',
+    person: 'Albert Einstein'
+  },
+  {
+    quote: 'Do not try to be original, just try to be good.',
+    person: 'Paul Rand'
+  },
+  {
+    quote: 'The way get started is to quit talking and begin doing.',
+    person: 'Walt Disney'
+  },
+  {
+    quote: 'Anyone who has never made a mistake has never tried anything new.',
+    person: 'Walt Disney'
+  },
+  {
+    quote: 'Your time is limited, so don’t waste it living someone else’s life.',
+    person: 'Albert Einstein'
+  },
+]
+
+export default quotes;
